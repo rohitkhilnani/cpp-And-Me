ba9db3e6f773c741569a77d3c2c63b2ce26f1c8a
rohitkhilnani <rohitkhilnani@ymail.com>
2016-01-17 15:23:35 -0500

    Add insertion sort

---
+++ sort/sort.h
@@ -0,0 +1,20 @@
+/*
+	This header defines several sorting algorithms on std::vector of int
+*/
+
+#ifndef __SORT_SORT__
+#define __SORT_SORT__
+
+#include<vector>
+
+// Sorts vector of int using insertion sort.
+// Time Complexity: O(n*n)
+// Space Complexity: O(1)
+// In place
+// Stable	
+
+void insertionSort(std::vector<int> &A);
+
+
+
+#endif	
