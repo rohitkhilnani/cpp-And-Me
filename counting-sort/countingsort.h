4d702b35a22dc4a4ed48cad0d89cb0874c093f85
rohitkhilnani <rohitkhilnani@ymail.com>
2016-01-18 15:06:34 -0500

    Add counting sort - linear time

---
+++ counting-sort/countingsort.h
@@ -0,0 +1,27 @@
+/*
+	This header defines counting sort on std::vector of int
+*/
+
+#ifndef __COUNTING_SORT__
+#define __COUNTING_SORT__
+
+#include<vector>
+#include<unordered_map>
+#include<iostream>
+
+class CountingSort{
+
+public:
+
+	// Sorts vector A using counting sort
+	// Run time: O(n + k)
+	// n - size of the vector
+	// k - upper bound on the values contained in vector A.
+
+	static void sort(std::vector<int> &A, int k);
+
+};
+
+
+
+#endif	
